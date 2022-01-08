ccb2fdffbb3014f1d453a8c6e7d1bd3e09fe5b8a
Alberto Avelino <hugo.avelinoh@gmail.com>
2022-01-07 21:23:14 -0600

    Replacings namespaces to fix unmarshalling issue

--- src/main/java/com/mx/ntlink/client/NtLinkClientImpl.java
+++ src/main/java/com/mx/ntlink/client/NtLinkClientImpl.java
@@ -3,16 +3,32 @@ package com.mx.ntlink.client;
 
 import com.mx.ntlink.client.generated.*;
 import com.mx.ntlink.error.SoapClientException;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /** @author Ralph */
+@Slf4j
 public class NtLinkClientImpl extends AbstractSoapClient implements NtLinkClient {
 
+  private static  final String I_SERVICIO_TIMBRADO = "/IServicioTimbrado";
+
   private static final String NTLINK_NAMESAPCE =
-      "https://ntlink.com.mx/IServicioTimbrado/IServicioTimbrado";
+          "https://ntlink.com.mx/IServicioTimbrado";
+
+  private static final String CERTIFICADOR_NAMESPACE =
+          "http://schemas.datacontract.org/2004/07/CertificadorWs";
+  private static final String CERTIFICADOR_BUSINESS_NAMESPACE =
+          "http://schemas.datacontract.org/2004/07/CertificadorWs.Business";
 
   public NtLinkClientImpl(String wsUrl) {
-    super(wsUrl, NTLINK_NAMESAPCE);
+    super(wsUrl, NTLINK_NAMESAPCE.concat(I_SERVICIO_TIMBRADO));
   }
 
   @Override
@@ -38,7 +54,7 @@ public class NtLinkClientImpl extends AbstractSoapClient implements NtLinkClient
   @Override
   public TimbraCfdiQrResponse timbrarCfdiConQr(TimbraCfdiQr request) throws SoapClientException {
     request.setComprobante("<![CDATA[" + request.getComprobante() + "]]>");
-    SOAPMessage response = sendRequest(request, TimbraCfdiQr.class);
+    SOAPMessage response = replaceNamespaces(sendRequest(request, TimbraCfdiQr.class));
     return parseResponse(response, TimbraCfdiQrResponse.class);
   }
 
@@ -54,7 +70,7 @@ public class NtLinkClientImpl extends AbstractSoapClient implements NtLinkClient
   public TimbraCfdiQrSinSelloResponse timbrarSinSelloConQr(TimbraCfdiQrSinSello request)
       throws SoapClientException {
     request.setComprobante("<![CDATA[" + request.getComprobante() + "]]>");
-    SOAPMessage response = sendRequest(request, TimbraCfdiQrSinSello.class);
+    SOAPMessage response = replaceNamespaces(sendRequest(request, TimbraCfdiQrSinSello.class));
     return parseResponse(response, TimbraCfdiQrSinSelloResponse.class);
   }
 
@@ -70,7 +86,7 @@ public class NtLinkClientImpl extends AbstractSoapClient implements NtLinkClient
   public TimbraRetencionQrResponse timbraRetenionConQr(TimbraRetencionQr request)
       throws SoapClientException {
     request.setComprobante("<![CDATA[" + request.getComprobante() + "]]>");
-    SOAPMessage response = sendRequest(request, TimbraRetencionQr.class);
+    SOAPMessage response = replaceNamespaces(sendRequest(request, TimbraRetencionQr.class));
     return parseResponse(response, TimbraRetencionQrResponse.class);
   }
 
@@ -80,5 +96,20 @@ public class NtLinkClientImpl extends AbstractSoapClient implements NtLinkClient
     request.setComprobante("<![CDATA[" + request.getComprobante() + "]]>");
     SOAPMessage response = sendRequest(request, TimbraRetencionSinSello.class);
     return parseResponse(response, TimbraRetencionSinSelloResponse.class);
+  }
+
+  private SOAPMessage replaceNamespaces(SOAPMessage message) throws SoapClientException {
+    try {
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      message.writeTo(bos);
+      String stringResponse = bos.toString("UTF-8");
+      stringResponse = stringResponse.replaceAll(CERTIFICADOR_NAMESPACE, NTLINK_NAMESAPCE);
+      stringResponse = stringResponse.replaceAll(CERTIFICADOR_BUSINESS_NAMESPACE, NTLINK_NAMESAPCE);
+      InputStream is = new ByteArrayInputStream(stringResponse.getBytes());
+      log.info("Replaced response: {}", stringResponse);
+      return MessageFactory.newInstance().createMessage(message.getMimeHeaders(), is);
+    } catch (IOException | SOAPException e) {
+      throw new SoapClientException("Error replacing namespaces" + e.getMessage());
+    }
   }
 }
