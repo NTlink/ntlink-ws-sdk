9b479ac5261dafb669ab10566ac6ab59920d7230
NTlink dev <105874218+ntlinkdev@users.noreply.github.com>
2022-11-17 16:34:26 -0600

    Agrego exception personalizada a TimbreFiscalDigitalTransformer

---
+++ src/main/java/com/mx/ntlink/error/TimbreFiscalDigitalException.java
@@ -0,0 +1,11 @@
+package com.mx.ntlink.error;
+
+public class TimbreFiscalDigitalException extends Exception {
+  public TimbreFiscalDigitalException(String message) {
+    super(message);
+  }
+
+  public TimbreFiscalDigitalException(String message, Throwable t) {
+    super(message, t);
+  }
+}
--- src/main/java/com/mx/ntlink/helper/TimbreFiscalDigitalTransformer.java
+++ src/main/java/com/mx/ntlink/helper/TimbreFiscalDigitalTransformer.java
@@ -1,5 +1,6 @@
 package com.mx.ntlink.helper;
 
+import com.mx.ntlink.error.TimbreFiscalDigitalException;
 import com.mx.ntlink.models.generated.TimbreFiscalDigital;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -8,13 +9,18 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 public class TimbreFiscalDigitalTransformer {
-  public static TimbreFiscalDigital xmlToCfdiModel(InputStream xmlStream) throws JAXBException {
-    JAXBContext jc = JAXBContext.newInstance(TimbreFiscalDigital.class);
-    Unmarshaller um = jc.createUnmarshaller();
-    return (TimbreFiscalDigital) um.unmarshal(xmlStream);
+  public static TimbreFiscalDigital xmlToCfdiModel(InputStream xmlStream)
+      throws TimbreFiscalDigitalException {
+    try {
+      JAXBContext jc = JAXBContext.newInstance(TimbreFiscalDigital.class);
+      Unmarshaller um = jc.createUnmarshaller();
+      return (TimbreFiscalDigital) um.unmarshal(xmlStream);
+    } catch (JAXBException ex) {
+      throw new TimbreFiscalDigitalException(ex.getMessage());
+    }
   }
 
-  public static TimbreFiscalDigital xmlToCfdiModel(String xml) throws JAXBException {
+  public static TimbreFiscalDigital xmlToCfdiModel(String xml) throws TimbreFiscalDigitalException {
     return xmlToCfdiModel(new ByteArrayInputStream(xml.getBytes()));
   }
 }
