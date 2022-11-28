630f6cbe50de0de88b64ea7cda042a61003d1bbc
havelino <hugo.avelino@ntlink.com.mx>
2022-11-27 22:02:48 -0500

    2.0.12 Homologating Transformer exceptions

--- src/main/java/com/mx/ntlink/helper/CfdiTransformer.java
+++ src/main/java/com/mx/ntlink/helper/CfdiTransformer.java
@@ -21,26 +21,35 @@ import javax.xml.bind.Unmarshaller;
 
 public class CfdiTransformer {
 
-  public static String cfdiMoldelToString(Comprobante cfdi) throws JAXBException {
-    JAXBContext jaxbContext = JAXBContext.newInstance(Comprobante.class);
-    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-    jaxbMarshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new CfdiNamespaceMapper());
-    jaxbMarshaller.setProperty(JAXB_FORMATTED_OUTPUT, true);
-    jaxbMarshaller.setProperty(
-        JAXB_SCHEMA_LOCATION,
-        String.format("%s %s %s %s", SAT_NS_40, SAT_XSD_40, PAGOS_NS, TFD_NS));
-    StringWriter sw = new StringWriter();
-    jaxbMarshaller.marshal(cfdi, sw);
-    return sw.toString();
+  public static String cfdiMoldelToString(Comprobante cfdi) throws XMLParserException {
+    try {
+      JAXBContext jaxbContext = JAXBContext.newInstance(Comprobante.class);
+      Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+      jaxbMarshaller.setProperty(
+          "com.sun.xml.bind.namespacePrefixMapper", new CfdiNamespaceMapper());
+      jaxbMarshaller.setProperty(JAXB_FORMATTED_OUTPUT, true);
+      jaxbMarshaller.setProperty(
+          JAXB_SCHEMA_LOCATION,
+          String.format("%s %s %s %s", SAT_NS_40, SAT_XSD_40, PAGOS_NS, TFD_NS));
+      StringWriter sw = new StringWriter();
+      jaxbMarshaller.marshal(cfdi, sw);
+      return sw.toString();
+    } catch (JAXBException ex) {
+      throw new XMLParserException(ex.getMessage());
+    }
   }
 
-  public static Comprobante xmlToCfdiModel(InputStream xmlStream) throws JAXBException {
-    JAXBContext jc = JAXBContext.newInstance(Comprobante.class);
-    Unmarshaller um = jc.createUnmarshaller();
-    return (Comprobante) um.unmarshal(xmlStream);
+  public static Comprobante xmlToCfdiModel(InputStream xmlStream) throws XMLParserException {
+    try {
+      JAXBContext jc = JAXBContext.newInstance(Comprobante.class);
+      Unmarshaller um = jc.createUnmarshaller();
+      return (Comprobante) um.unmarshal(xmlStream);
+    } catch (JAXBException ex) {
+      throw new XMLParserException(ex.getMessage());
+    }
   }
 
-  public static Comprobante xmlToCfdiModel(String xml) throws JAXBException {
+  public static Comprobante xmlToCfdiModel(String xml) throws XMLParserException {
     return xmlToCfdiModel(new ByteArrayInputStream(xml.getBytes()));
   }
 
